82f995c52adb7847218e2dce7d3517ce6a0b6fdc
MalvinJu <malvinjuanda@yahoo.com>
2017-02-04 21:36:44 +0700

    initial push from chalvin

---
+++ Color.cpp
@@ -0,0 +1,26 @@
+#include "Color.h"
+
+Color::Color(){
+	setColor(0,0,0);
+}
+
+Color::Color(int r, int g, int b){
+	setColor(r,g,b);
+}
+
+void Color::setColor(int r, int g, int b){
+	Red = r;
+	Green = g;
+	Blue = b;
+}
+int Color::getRed(){
+	return Red;
+}
+int Color::getGreen(){
+	return Green;
+}
+int Color::getBlue(){
+	return Blue;
+}
+
+
---
+++ Screen.cpp
@@ -0,0 +1,64 @@
+#include "Screen.h"
+
+Screen::Screen(){
+	fb_fd = open("/dev/fb0",O_RDWR);
+
+	//Get variable screen information
+	ioctl(fb_fd, FBIOGET_VSCREENINFO, &vinfo);
+	ioctl(fb_fd, FBIOGET_FSCREENINFO, &finfo);
+	//set grayscale into color mode and set bits per pixel
+	vinfo.grayscale=0;
+	vinfo.bits_per_pixel=32;
+	ioctl(fb_fd, FBIOPUT_VSCREENINFO, &vinfo);
+
+	//setting up the buffer pointer
+	int buffersize = vinfo.yres_virtual * finfo.line_length;
+	fbp = (char *) mmap(0, buffersize, PROT_READ | PROT_WRITE, MAP_SHARED, fb_fd, (off_t)0);
+
+	
+
+}
+
+
+Color Screen::getColor(int r, int c){
+	int location = (c+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (r+vinfo.yoffset) * finfo.line_length;
+	int red = *(fbp + location);
+	int green = *(fbp + location + 1);
+	int blue = *(fbp + location + 2);
+	return Color(red,green,blue);
+}
+void Screen::setColor(int r, int c, int red, int green, int blue){
+	int location = (c+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (r+vinfo.yoffset) * finfo.line_length;
+	*(fbp + location) =red;  
+	*(fbp + location +1) = green;
+	*(fbp + location +2) = blue;
+	*(fbp + location +2) = 0;
+}
+
+void Screen::setColor(int r, int c, Color color){
+	int location = (c+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (r+vinfo.yoffset) * finfo.line_length;
+	*(fbp + location) =color.getRed();  
+	*(fbp + location +1) = color.getGreen();
+	*(fbp + location +2) = color.getBlue();
+	*(fbp + location +3) = 0;
+}
+
+int Screen::getWidth(){
+	return vinfo.xres;
+}
+int	Screen::getHeight(){
+	return vinfo.yres;
+}
+
+void Screen::ClearScreen(){
+	for (int r=0;r<vinfo.yres;r++)
+	{
+		for (int c=0;c<vinfo.xres;c++){
+			int location = (c+vinfo.xoffset) * (vinfo.bits_per_pixel/8) + (r+vinfo.yoffset) * finfo.line_length;
+			*(fbp + location) = 0;
+			*(fbp + location +1) = 0;
+			*(fbp + location +2) = 0;
+			*(fbp + location + 3) = 0;
+		}
+	}
+}
